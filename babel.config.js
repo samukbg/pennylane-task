c2ac261984187ae5630d1aff7ac66578b3045929
Samuel Gomes <samukbg@msn.com>
2026-01-13 01:02:03 +0200

    Fixing babel duplicate bug

--- babel.config.js
+++ babel.config.js
@@ -4,23 +4,20 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
-        'module:react-native-dotenv', {
-          moduleName: '@env',
-          path: '.env',
-        },
         '@tamagui/babel-plugin',
         {
           components: ['tamagui'],
           config: './src/ui/tamagui.config.ts',
           logTimings: true,
           disableExtraction: process.env.NODE_ENV === 'development',
-        }
+        },
       ],
       [
-        'module:react-native-dotenv', {
+        'module:react-native-dotenv',
+        {
           moduleName: '@env',
           path: '.env',
-        }
+        },
       ],
     ],
   };
